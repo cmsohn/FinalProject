61561b502e29110af9c6d691f987129e4044645d
cmsohn <71651503+cmsohn@users.noreply.github.com>
2020-12-12 18:49:13 -0600

    Add Final_Project

---
+++ Final_Project/ASSETS/JS/NavEagle.js
@@ -0,0 +1,15 @@
+function switchImage() {
+    var image = document.getElementById('route');
+    image.src="ASSETS/Images/newgianteagle5.png"
+}
+
+function switchImageII() {
+    var image = document.getElementById('final');
+    image.src="ASSETS/Images/newgianteagle6.png"
+}
+
+function switchImageIII() {
+    var image = document.getElementById('alert');
+    image.src="ASSETS/Images/newgianteagle4.png"
+}
+
---
+++ Final_Project/ASSETS/JS/main.js
@@ -0,0 +1,13 @@
+//function to enact typewriter action from Typed.JS library
+function type() {
+
+  var typed2 = new Typed('#main', {
+    strings: ['HELLO.','CHRISTIE SOHN'],
+    typeSpeed: 75,
+    backSpeed: 0,
+    fadeOut: true,
+    loop: false,
+  });
+}
+
+
