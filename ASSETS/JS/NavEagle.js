27e8eb76583fdef4062c1d4419936ea5f9108dca
cmsohn <71651503+cmsohn@users.noreply.github.com>
2020-12-13 11:56:44 -0600

    final project update

--- ASSETS/JS/NavEagle.js
+++ ASSETS/JS/NavEagle.js
@@ -1,13 +1,16 @@
+//function to change image after button click
 function switchImage() {
     var image = document.getElementById('route');
     image.src="ASSETS/Images/newgianteagle5.png"
 }
 
+//function to change image after button click
 function switchImageII() {
     var image = document.getElementById('final');
     image.src="ASSETS/Images/newgianteagle6.png"
 }
 
+//function to change image after button click
 function switchImageIII() {
     var image = document.getElementById('alert');
     image.src="ASSETS/Images/newgianteagle4.png"
